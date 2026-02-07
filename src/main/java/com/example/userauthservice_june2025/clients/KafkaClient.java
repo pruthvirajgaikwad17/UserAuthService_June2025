e3c884c2ad50f58e024b1f0aea4c00c7ad4969ba
PruthvirajGaikwad <pruthvirajgaikwad1717@gmail.com>
2026-02-07 12:20:50 +0530

    send email using kafka

--- src/main/java/com/example/userauthservice_june2025/clients/KafkaClient.java
+++ src/main/java/com/example/userauthservice_june2025/clients/KafkaClient.java
@@ -1,4 +1,22 @@
 package com.example.userauthservice_june2025.clients;
 
+import org.springframework.kafka.core.KafkaTemplate;
+import org.springframework.stereotype.Component;
+
+@Component
 public class KafkaClient {
+
+    // These will have two argument
+    // 1. Topic (string)
+    // 2. Message (Json in form of string)
+    private KafkaTemplate<String, String> kafkaTemplate;
+
+    public KafkaClient(KafkaTemplate<String, String> kafkaTemplate) {
+        this.kafkaTemplate = kafkaTemplate;
+    }
+
+    public void sendMessage(String topic,String message){
+        kafkaTemplate.send(topic, message);
+    }
+
 }
